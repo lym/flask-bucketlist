b260b155d09d3b351452a32b4e717c27c58c8945
Salym Senyonga <salymsash@gmail.com>
2017-07-12 14:24:15 +0300

    Add initial models

---
+++ app/models/__init__.py
@@ -0,0 +1,13 @@
+from .bucketlist import Bucketlist
+from .item import Item
+from .user import User
+from .bl_ds import BlDataStore
+from .base_model import BaseModel
+
+__all__ = [
+    'Bucketlist',
+    'User',
+    'Item',
+    'BlDataStore',
+    'BaseModel'
+]
---
+++ app/models/bucketlist.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+import uuid
+from tinydb import where
+
+from .base_model import BaseModel
+
+
+class Bucketlist(BaseModel):
+    table_name = 'bucketlists'
+
+    def __init__(
+        self, title, description='', fulfilled=False
+    ):
+        # super().__init__()
+        self.title = title
+        self.description = description
+        self.fulfilled = fulfilled
+
+    @classmethod
+    def create(cls, **kwargs):
+        db_table = cls.db.table(cls.table_name)
+        if kwargs is None or len(kwargs) == 0:
+            return
+        kwargs['created'] = datetime.now(cls.tz)
+        kwargs['modified'] = datetime.now(cls.tz)
+        for key, value in kwargs.items():
+            kwargs[key] = value
+        db_table.insert(
+            {
+                'id': int(uuid.uuid4()), 'data': kwargs
+            }
+        )
+
+    @classmethod
+    def delete(cls, _id):
+        print('ID of Item for deletion'.format(_id))
+        table = cls.db.table(cls.table_name)
+        table.remove(where('id') == _id)
+
+    """
+    @classmethod
+    def fetch_record(cls, _id):
+        table = self.db.table(self.table_name)
+        record = table.get(where('id') == _id)
+        result = {}
+        result['id'] =
+        result['created'] =
+        result['modified'] =
+    """
