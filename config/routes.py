2ec116d569e11d5bfc6410fc409c240bb6100004
Salym Senyonga <salymsash@gmail.com>
2017-07-13 10:41:40 +0300

    Backend: Update router configuration
    Signed-off-by: Salym Senyonga <salymsash@gmail.com>

--- config/routes.py
+++ config/routes.py
@@ -1,12 +1,15 @@
 from app.controllers import (
     HomeController,
     UsersController,
+    LoginController,
 )
 from config.application import App
 
-App.add_url_rule('/home/', view_func=HomeController.as_view('home'))
+App.add_url_rule('/', view_func=HomeController.as_view('home'))
 
 App.add_url_rule(
     '/users/',
     view_func=UsersController.as_view('user-login')
 )
+
+App.add_url_rule('/login/', view_func=LoginController.as_view('login'))
