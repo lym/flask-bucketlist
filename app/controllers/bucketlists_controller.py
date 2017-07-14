8b36093c484f8c19cfa838c463c9840f66ed50df
Salym Senyonga <salymsash@gmail.com>
2017-07-14 08:02:24 +0300

    Enable bucketlise search-by-id
    Signed-off-by: Salym Senyonga <salymsash@gmail.com>

--- app/controllers/bucketlists_controller.py
+++ app/controllers/bucketlists_controller.py
@@ -3,6 +3,7 @@ from flask import (
     request,
 )
 from flask.views import MethodView
+from tinydb import where
 
 from app.models import Bucketlist
 
@@ -22,4 +23,11 @@ class BucketlistsController(MethodView):
 
     def get(self):
         bucketlists = jsonify(Bucketlist.all())
+        if request.args.get('item') is not None:
+            bucketlist_id = int(request.args.get('item'))
+            table = Bucketlist.db.table('bucketlists')
+            result = table.get(where('id') == bucketlist_id)
+            if result is None:
+                return jsonify({})
+            return jsonify(result)
         return bucketlists
