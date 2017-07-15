f73c6ba401b01a0f39cf3756167b1c9b6be1a79e
Salym Senyonga <salymsash@gmail.com>
2017-07-15 19:54:32 +0300

    Add user validation in login controller
    Signed-off-by: Salym Senyonga <salymsash@gmail.com>

--- app/controllers/login_controller.py
+++ app/controllers/login_controller.py
@@ -1,14 +1,20 @@
 from flask import (
+    abort,
     jsonify,
     request,
 )
 from flask.views import MethodView
+
+from app.models import User
 
 
 class LoginController(MethodView):
     def post(self):
         email = request.form.get('email')
         passw = request.form.get('password')
+        if User.valid_user(email, passw):
+            res = {'email': email, 'status': 'OK'}
+            return jsonify(res)
+
         print('Email: {}\nPass: {}'.format(email, passw))
-        res = {'email': email, 'status': 'OK'}
-        return jsonify(res)
+        abort(401)
